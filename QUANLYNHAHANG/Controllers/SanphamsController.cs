f7bdbac0f3c82163e1ae77bbe6bdb420695ab64c
187PM20621 - Vo Minh Vuong - K24T-PM1 <vuong.187pm20621@vanlanguni.vn>
2021-06-30 22:43:27 +0700

    Tao chức năng xem chji tiết và tìm kiém sản phẩm

--- QUANLYNHAHANG/Controllers/SanphamsController.cs
+++ QUANLYNHAHANG/Controllers/SanphamsController.cs
@@ -23,26 +23,25 @@ namespace QUANLYNHAHANG.Controllers
             return View(sanphams.ToList());
         }
         [AllowAnonymous]
-        // cho khách hàng
-        public ActionResult Index2()
+        public ActionResult Search(String keyword)
         {
-            var sanphams = db.Sanphams.Include(s => s.Loaisanpham);
-            return View(sanphams.ToList());
-        }
+            var model = db.Sanphams.ToList();
+            model = model.Where(p => p.Tên_món_ăn.ToLower().Contains(keyword.ToLower())).ToList();
+            ViewBag.Keyword = keyword;
+            return View("Index2", model);
 
+        }
         // GET: Sanphams/Details/5
+        [AllowAnonymous]
         public ActionResult Details(string id)
+
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Sanpham sanpham = db.Sanphams.Find(id);
-            if (sanpham == null)
+            var model = db.Sanphams.Find(id);
+            if (model == null)
             {
                 return HttpNotFound();
             }
-            return View(sanpham);
+            return View(model);
         }
 
         // GET: Sanphams/Create
