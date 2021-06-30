5f40a9558179745431abbf8f30b8055adbf46b8f
197PM09506 - Nguyễn Mạnh Tài - K25T-PM <tai.197pm09506@vanlanguni.vn>
2021-06-30 21:07:57 +0700

    Thê quyền và phân quyền cho người dùng

--- QUANLYNHAHANG/Models/Model1.Designer.cs
+++ QUANLYNHAHANG/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\document\Nhahang1\QUANLYNHAHANG\QUANLYNHAHANG\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Admin\Source\Repos\QUANLYNHAHANG1\QUANLYNHAHANG\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
